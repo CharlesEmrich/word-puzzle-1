aa4d9a55b6dd5617352b03ac19dd1fc6c0b8d6ab
eluts <eluts15@gmail.com>
2017-05-02 21:49:20 -0700

    add comments, acknowledge a bug in the instance if nothing is entered and submitted, the form is sent anyway

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,22 @@
+//TODO:Current Bug- if nothing is entered, and form is submitted the box will still submit.
+
+
+//fun stuff
+function replaceVowels (input) {
+
 var vowels = ["a","e","i","o","u"];
 
-function replaceVowels (input) {
-  var output = input;
+  var output = input; //allocate memory, pass it on to output
   for (var i = 0; i < input.length; i++) {
-    for (var ii = 0; ii < vowels.length; ii++) {
-      if (input[i] === vowels[ii]) {
-        output = output.replace(input[i],"-");
+    for (var ii = 0; ii < vowels.length; ii++) { //really like the ii notation, indicating the iterating the inner for loop. If there was another inner loop it would be indicated by iii
+      if (input[i] === vowels[ii]) { //if the element in the array matches a character from the array of values, are the same... do this
+        output = output.replace(input[i]," Vim "); //yes(: replace any instance of a vowel is replaced with the word vim.
       }
     }
   }
-  return output;
+  return output; 
 }
+
 
 $(function() {
   $("form").submit(function(event) {
